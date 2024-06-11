3ce037beff9f03b5d886dfa768e9bd5c8eff0f09
bradenross <bradenro55@yahoo.com>
2024-06-11 16:04:02 -0500

    Initial Commit

---
+++ ChristmasCountdown/ChristmasCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasCountdownApp.swift
+//  ChristmasCountdown
+//
+//  Created by Braden Ross on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
